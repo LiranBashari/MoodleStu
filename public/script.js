5ffd4414ab0b9edc98369f04204d54fe44f8a940
LiranBashari <86421529+LiranBashari@users.noreply.github.com>
2022-07-22 18:46:25 +0300

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,15 @@
+// to do: add the details from DB to the web by id
+// to do: query to database to take the courses of the specific student access with the details
+
+
+
+function addExistCourse(){
+    // display to the user the list of the other courses-> by click add it to the courses list
+    // and add the course to the DB
+    alert("add")
+}
+
+function addNewCourse(){
+    // display some form that create a new course and then add to DB and to the List.
+    alert("new")
+}
